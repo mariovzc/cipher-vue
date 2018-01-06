1bf5be866825a2a30938dae1de8f356ae5adafd0
Mario vizcaino <mario.vizcainos@gmail.com>
2018-01-06 16:35:59 -0500

    setup the cipher form and some methods

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,59 @@
 <template>
   <div>
-    <h1>Cifrado</h1>
+    <v-app>
+      <v-container grid-list-md text-xs-center>
+        <h1>Cifrado</h1>
+        <v-layout row wrap>
+          <v-flex xs6 offset-xs3>
+            <v-text-field
+              label="Palabra"
+              v-model="word"
+              required
+              :rules="wordRules"
+            ></v-text-field>
+            <v-slider 
+              :label="slider.label" 
+              min="0" max="26" 
+              v-model="code" 
+              :color="slider.color"
+              thumb-label
+            ></v-slider>
+            <v-radio-group 
+              v-model="type" 
+              :mandatory="false" 
+              row
+            >
+              <v-radio 
+                label="Cifrar" 
+                value="Cifrar" 
+                color="primary"
+              ></v-radio>
+              <v-radio 
+                label="Descifrar" 
+                value="Descifrar" 
+                color="success"
+              ></v-radio>
+            </v-radio-group>
+            <v-btn  
+              v-if="type" 
+              color="primary"
+              @click="convert"
+            >
+              {{type}}
+            </v-btn>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-app>
+    <v-snackbar
+      :right="true"
+      :top="true"
+      v-model="snackbar"
+      :absolute="true"
+    >
+      Error Verifique que la palabra este en el formato correcto
+      <v-btn flat color="pink" @click.native="snackbar = false">Cerrar</v-btn>
+    </v-snackbar>
   </div>
 </template>
 
@@ -9,6 +62,23 @@ export default {
   name: 'Main',
   data () {
     return {
+      word: '',
+      valid: false,
+      wordRules: [
+        (v) => !!v || 'El campo es obligatorio',
+        (v) => /^[a-zA-Z\s]*$/.test(v) || 'Solo pueden ser letras'
+      ],
+      code: 0,
+      type: '',
+      slider: {
+        label: 'Codigó:', color: 'orange darken-3'
+      },
+      snackbar: true
+    }
+  },
+  methods: {
+    convert () {
+      console.log('ok')
     }
   }
 }
